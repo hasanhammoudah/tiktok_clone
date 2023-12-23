045efc289739f3cb165984f3395508c4f2c2c90b
hasanhammoudah <https://github.com/hasanhammoudah/Dynamic-Chart-in-PHP>
2023-12-23 21:11:05 +0300

    NEW UPDATE

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,10 +1,44 @@
 import 'dart:io';
-
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/models/user.dart' as model;
 
 class AuthContoller extends GetxController {
-  // registering the user
+  // upload to firebase storage
 
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  // registering the user
   void registerUser(
-      String username, String email, String password, File? image) {}
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty && password.isNotEmpty && image != null) {
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        String downloadUrl = await _uploadToStorage(image);
+        model.User user = model.User(
+            name: username,
+            profilePhoto: downloadUrl,
+            email: email,
+            uid: cred.user!.uid);
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error Creating Account',
+        e.toString(),
+      );
+    }
+  }
 }
